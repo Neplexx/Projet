343ae4d6979d106f7329138aca8b8c58713d8f1e
Corentin <ivillegas@gmx.fr>
2025-11-17 12:17:52 +0100

    z

--- fichiers/main.cpp
+++ fichiers/main.cpp
@@ -19,3 +19,29 @@ using namespace sf;
 
 const std::string path_image(PATH_IMG);
 
+int main() {
+
+
+    RenderWindow app(VideoMode({ 600, 400 }, 32), "My Camera");
+
+    app.setFramerateLimit(60); // limite la fenêtre à 60 images par seconde
+
+    //Texture backgroundImage(path_image + "C:/Users/ivill/source/repos/Projet/fichiers/assets/France.jpg", false, sf::IntRect({ 0, 0 }, { 600, 400 }));
+
+    //Sprite backgroundSprite(backgroundImage);
+
+    while (app.isOpen())
+    {
+        // Gestion des événements SFML 3
+        while (auto event = app.pollEvent())
+        {
+            if (event->is<sf::Event::Closed>())
+                app.close();
+        }
+
+        app.clear();   // Vide la fenêtre
+        app.display(); // Affiche le contenu (ici : rien)
+    }
+
+    return 0;
+}
