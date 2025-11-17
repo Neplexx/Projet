91c0aa5994437b5142dc8ab80011e75ea89bc5d5
Corentin <ivillegas@gmx.fr>
2025-11-17 12:07:41 +0100

    osef zehef

---
+++ fichiers/main.cpp
@@ -0,0 +1,21 @@
+#include <cstdlib>
+#include <iostream>
+#include <vector>
+#include <memory>
+#include <SFML/Graphics.hpp>
+
+//#include "Plane.hpp"
+
+using namespace std;
+using namespace sf;
+
+#ifdef _MSC_VER
+#pragma comment(linker, "/SUBSYSTEM:windows /ENTRY:mainCRTStartup")
+#define _PATH_IMG_ "C:/Program Files/SFML/img/"
+#else
+// #define _PATH_IMG_ "../img/"
+#define _PATH_IMG_ "./img/"
+#endif
+
+const std::string path_image(PATH_IMG);
+
